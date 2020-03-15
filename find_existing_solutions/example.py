5e35804fd80f1912dd3d745146778ab30fb5b797
outdreamer <jonijezewski@gmail.com>
2020-03-14 19:18:22 -0700

    add workflow example interface

---
+++ find_existing_solutions/example.py
@@ -0,0 +1,106 @@
+# example.py
+
+
+'''
+This file contains example problem-solving automation workflows & example data for those workflows.
+'''
+
+'''
+Workflow 1: Identify problem metadata (like type), query for solutions to adjacent problems, transform into adjacent problem if found, solve, and convert to original problem
+	
+	Example: solving a multiplication problem can be framed as an adjacent multiplication problem with a pre-computed solution & an addition/subtraction problem to convert to original multiplication problem.
+
+'''
+
+	'''
+	Workflow 1a: Transform into combination of solved problems.
+
+		Example: social inequality can be framed as a logistics problem (optimal transport, efficiency-creation)
+
+	'''
+
+'''
+Workflow 2: Describe interface query for problem/solution.
+
+	Example: problem uses set of types, intents, structures and solution uses information, functions, and layers to address dimensions of problem.
+
+'''
+
+'''
+Workflow 3: Convert to information layer problem & find matching information layer solution, then convert information layer solution to structural-informational solution implementation.
+	
+	Example: converting the "building a website" problem into an information problem: "finding right tools and tool-assembly tools and tool-automation tools", which can be broken into 
+	"correct info asymmetry" problems, "organize information" problems, "distribute information" problems, etc.
+
+'''
+
+'''
+Workflow 4: Convert to function-intent layer problem & find matching intent layer solution, then convert intent layer solution to structural-informational solution implementation.
+
+	Example: indexing functions by intent converts the "building a website" problem into a function structure matching & alignment problem, as well as a problem of organizing function access by intent.
+
+'''
+
+	'''
+	Workflow 4a: Vectorize by input/output/intent
+
+		Example: indexing functions by intent converts the "reduce duplicate lines in these files" problem into a logical information filter problem.
+
+	'''
+
+'''
+Workflow 5: Convert to structure layer problem & find matching structure layer solution, then convert structure layer solution to structural-informational solution implementation.
+
+	Example: Match attributes of problem with structure (such as a circle system), then find structure that solves the problem structure (like a triangle system), and convert solution structure to implementation on the combined structural-information interface.
+
+'''
+
+'''
+Workflow 6: Convert to concept layer problem & find matching concept layer solution, then convert concept layer solution to structural-informational solution implementation.
+
+	Example: Start with concepts required or start with structure required, then convert to other interface with interface query (or directly if concepts are already mapped to structures in your resource set).
+
+'''
+
+'''
+Workflow 7: Convert to pattern interface (solution patterns, problem patterns, problem-solution matching patterns) starting with known patterns for this problem type & core functions of solutions.
+
+	Example: Applying sub-system patterns, applying matching patterns, applying structure-finding patterns, etc - as a way to automate logic in the absence of specific logic coded for this solution.
+
+'''
+
+'''
+Workflow 8: Convert to causal interface & derive solution query on that interface, then apply causal solution query to find structural-informational solution implementation.
+
+	Example: Identify causal shapes (or network of possible shapes), then resolve causal trajectories on those shapes to find causal stack of a problem attribute/object/rule.
+
+'''
+
+'''
+Workflow 9: Convert to attribute interface, converting problem & available solutions to attribute vector sets, then match neutralizing attribute vector sets after checking impact on problem space.
+
+	Example: Identify attribute metadata (attribute type, causation, cooperation, variance patterns & shapes, probability distribution, potential, etc) and convert attributes to vector network for the problem attributes, then find matching solution attribute networks that could neutralizing the problem attribute network.
+
+'''
+
+'''
+Workflow 10: Identify (abstract/contextual) interface combination that will prove most useful for solving this problem, frame problem on those interfaces, and use interface queries to find solution query, then convert solution query to structural-informational solution implementation.
+	
+	Example: The problem statement gives information on the type/attribute interfaces, so start by framing the problem on those interfaces as well as the pattern interface if there are any type/attribute patterns stored.
+
+'''
+
+'''
+Workflow 11: Derive & describe system in which problem occurs using system analysis (identify core functions, vertices, conflicts, structures (layers/limits/gaps), variance shapes & sub-systems) and interface query for that system, then solve problem by adjusting system.
+
+	Example: a system analysis insight like "if you make a boundary adjustment to this system, it solves a problem of variance injection at this injection point" can be applied once the system is described.
+
+'''
+
+'''
+Workflow 12: Analyze problem-solving strategy cost & value generated by solution, decide whether to solve problem, select a workflow from the above for a particular problem statement & resource set.
+
+	Example: the cost of all probable solutions is higher than value generated, but this problem (web security) will continue if unaddressed, so either solving the problem or a causative problem (web protocols) is necessary.
+
+'''
+
