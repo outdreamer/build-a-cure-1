854b17d60d5a8c2a1ca3e7049bce94310c77eeb9
outdreamer <jonijezewski@gmail.com>
2020-03-23 22:23:15 -0700

    add function interfaces

---
+++ find_existing_solutions/interface_functions.py
@@ -0,0 +1,36 @@
+def identify_interface(system):
+	''' identify interfaces in a system 
+		this includes:
+			- identifying relevant abstract interfaces
+			- identifying relevant general interfaces expected for a type 
+			- identifying relevant specific interfaces
+	'''
+	return False
+	
+def define_interface(system):
+	''' once an interface is identified, describe its generative objects/functions & store the definition 
+		the standard metadata for an interface:
+
+		- the attribute determining the interface filter set (type/intent/structure)
+		- the filter set (conversion functions)
+
+	'''
+	return False
+
+def convert_to_interface(system):
+	''' 
+	convert a system to an interface, deriving the interface-relevant objects for each system object/attribute/function,
+	and returning the set of derived objects as a converted system 
+		- this means applying the filter set in the definition of the interface 
+
+	'''
+	return False
+
+def match_interface(system):
+	''' this is usually just determining which interface an object/attribute/function is most relevant to or clearly a member of 
+
+	- if an object isnt listed as a member of an interface in interface_networks.json, the interface most likely to be relevant can be evaluated
+		- with testing functions (what does it act like, what is it similar to, what groups is it a member of, what is it definitely not
+		- apply a structure to the object, and ifnd what structure matches the most (is it a direction? thats probably an intent)
+	'''
+	return False
--- find_existing_solutions/system_functions.py
+++ find_existing_solutions/system_functions.py
@@ -11,13 +11,37 @@
     - attribute (like similarity) testing function
     - function application function
 '''
+system = [
+	'vdj recombination uses variable, diversity, and joining segments',
+	'these segments are recombinated using core operations like delete and copy',
+	'constant segments are also involved',
+	'antibodies or t-cell receptors can be formed using vdj recombination',
+]
+''' from this system described as a function set:
+	objects = [variable segment, diversity segment, joining segment, constant segment, antibodies, t-cell receptors, receptors, t-cells]
+	attributes = [constant, variable, diverse, joined, involved, core, like, possible/can, with]
+	functions = [vdj recombination, delete copy, recombine, combine, use/apply, apply process, create/form]
+'''
+random_set = [
+	'membranes',
+	'bonds',
+	'checkpoints',
+	'pressure',
+	'neutralizing process',
+	'competition'
+]
+''' out of this system described as a set of different type instances:
+	objects = [membranes, bonds, checkpoints, points, checks, pressure]
+	attributes = [limit, pressure, force, neutral, opposing]
+	functions = [neutralize, press/apply pressure, check, limit, connect, process, compete]
+'''
 
 def identify_objects(system):
 	''' system can be an object or a function or a set, like functions extracted from a paragraph describing the system '''
 	modifiers = ['processing']
 	agency_keywords = ['acts', 'exerts']
 	''' 
-		this will also apply general system patterns to isolate objects by predicting their existence & narrowing down their identities using filters 
+	- this will also apply general system patterns to isolate objects by predicting their existence & narrowing down their identities using filters 
 		
 		- given that we know that systems generally have layers, what objects act like layers or are there any traces of layers in the function set?
 			- if so, what other attributes do they have in common with the layer object
@@ -27,34 +51,7 @@ def identify_objects(system):
 		- this is applicable when we dont have a full description of the system or need to classify an object in the function set as the overriding object type 
 			- a membrane may not just be a boundary, it may also act as a filter, and that function may be more important so it should be classified as that
 	
-	'''
-	system = [
-		'vdj recombination uses variable, diversity, and joining segments',
-		'these segments are recombinated using core operations like delete and copy',
-		'constant segments are also involved',
-		'antibodies or t-cell receptors can be formed using vdj recombination',
-	]
-	''' from this system described as a function set:
-		objects = [variable segment, diversity segment, joining segment, constant segment, antibodies, t-cell receptors, receptors, t-cells]
-		attributes = [constant, variable, diverse, joined, involved, core, like, possible/can, with]
-		functions = [vdj recombination, delete copy, recombine, combine, use/apply, apply process, create/form]
-	'''
-
-	random_set = [
-		'membranes',
-		'bonds',
-		'checkpoints',
-		'pressure',
-		'neutralizing process',
-		'competition'
-	]
-	''' out of this system described as a set of different type instances:
-		objects = [membranes, bonds, checkpoints, points, checks, pressure]
-		attributes = [limit, pressure, force, neutral, opposing]
-		functions = [neutralize, press/apply pressure, check, limit, connect, process, compete]
-	'''
-
-	''' methods to identify objects:
+	- methods to identify objects:
 		- start by applying general system patterns to theorize existence of objects and see if any are found in expected positions
 		- replace theorized objects with other object keywords and see if the system still makes sense (is logically consistent, with no invalid rules)
 		- rule out functions/attributes
@@ -64,14 +61,13 @@ def identify_objects(system):
 
 def identify_attributes(system):
 	''' system can be an object or a function '''
-
 	''' attributes may need to be derived and arent as explicitly defined as often as objects '''
 
 	return False
 
 def identify_functions(system):
 	''' system can be an object or a function '''
-
 	''' functions may be easily mapped to verbs, but emerging functions need to be derived '''
 
 	return False
+
