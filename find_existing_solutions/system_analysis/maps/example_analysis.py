57cc3160fa5624a719877fb650af0ccb37535d59
outdreamer <jonijezewski@gmail.com>
2020-04-13 16:15:53 -0700

    add example system analysis patterns

--- find_existing_solutions/system_analysis/maps/example_analysis.py
+++ find_existing_solutions/system_analysis/maps/example_analysis.py
@@ -2,6 +2,11 @@
 
 In the patterns from system_analysis.json, you have example filters to use when reducing a problem before trying specific methods to solve it.
 
+These are the objects to look for in order to:
+	- identify causes of system error
+	- understand a system quickly
+	- optimize a system
+
 Examples of each filter follow:
 
 {
@@ -9,14 +14,68 @@ Examples of each filter follow:
 		"attribute cluster": "species differentiation features tend to cluster"
 		"attribute similarity": "location/agents (hospitals & staff/patients) with isolated conditions to prevent overuse of resource needed (cleaning supplies) for mixed condition (non-isolated hospitals/staff), where isolated conditions align with invalidation of cleaning supplies for that condition",
 		"attribute alignment": "rotation force aligned with input force creates momentum",
-		"attribute matching": "stacking objects that can make cubes in the knapsack problem to reduce unused space between objects"
+		"attribute matching": "stacking objects that can make cubes in the knapsack problem to reduce unused space between objects",
+		"emergent attribute": ""
 	},
 	"concept": {
+		"definition route": "",
+		"definition structure": "most abstract concepts have a definition network"
+	},
+	"error": {
 		"false similarity": "there are many routes to a shape or point which may differ on important metrics like intent",
+		"false contradiction": "",
+		"false potential": "",
+		"false constant": "",
+		"false conflict": ""
 	},
 	"state": {
 		"state permutation": "thermostat that switches off if natural temperature is equal to set involves permuting state of temperature variable to find a case where AC/heat wouldnt be needed for intent of conserving resources"
-
+	},
+	"type": {
+		"constant attribute set": "types are a set of attributes that stabilize long enough to be identifiable as a type and interact to form other types"
+	},
+	"problem": {
+		"conflict": "",
+		"competition": "",
+		"distribution": "",
+		"organization": "",
+		"imbalance": "",
+		"catalyst": ""
+	},
+	"logic": {
+		"implication": "",
+		"contradiction": "",
+		"condition": ""
+	},
+	"pattern": {
+		"repetition": "",
+		"order": "",
+		"position": "",
+		"function distortion": ""
+	},
+	"intent': {
+		"intent ambiguity": "",
+	},
+	"potential": {
+		"enforced rules": "",
+		"boundary gaps": ""
+	},
+	"change": {
+		"variance injection": "",
+		"attribute accretion": "",
+		"change demand/supply": "change occurs from triggers (phase shift, threshold, interaction) and the structures that can support them (matter state)"
+	},
+	"cause": {
+		"cause direction": "",
+		"alternative": ""
+	},
+	"interface": {
+		"perspective": "",
+		"standard": "",
+		"filter": ""
+	},
+	"function": {
+		"core function": ""
 	}
 }
 
