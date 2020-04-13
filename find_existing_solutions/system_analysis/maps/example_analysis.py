637ca9ef16ac96d78ff1998447856f6c4b145ca0
outdreamer <jonijezewski@gmail.com>
2020-04-13 15:55:47 -0700

    add example system analysis patterns

---
+++ find_existing_solutions/system_analysis/maps/example_analysis.py
@@ -0,0 +1,23 @@
+'''
+
+In the patterns from system_analysis.json, you have example filters to use when reducing a problem before trying specific methods to solve it.
+
+Examples of each filter follow:
+
+{
+	"attribute": {
+		"attribute cluster": "species differentiation features tend to cluster"
+		"attribute similarity": "location/agents (hospitals & staff/patients) with isolated conditions to prevent overuse of resource needed (cleaning supplies) for mixed condition (non-isolated hospitals/staff), where isolated conditions align with invalidation of cleaning supplies for that condition",
+		"attribute alignment": "rotation force aligned with input force creates momentum",
+		"attribute matching": "stacking objects that can make cubes in the knapsack problem to reduce unused space between objects"
+	},
+	"concept": {
+		"false similarity": "there are many routes to a shape or point which may differ on important metrics like intent",
+	},
+	"state": {
+		"state permutation": "thermostat that switches off if natural temperature is equal to set involves permuting state of temperature variable to find a case where AC/heat wouldnt be needed for intent of conserving resources"
+
+	}
+}
+
+'''
