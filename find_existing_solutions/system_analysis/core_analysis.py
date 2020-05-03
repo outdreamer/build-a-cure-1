e8cd02a96c43e20b5c71b4166dfa4bbad1b85c3c
outdreamer <jonijezewski@gmail.com>
2020-05-02 17:35:01 -0700

    add notes

--- find_existing_solutions/system_analysis/core_analysis.py
+++ find_existing_solutions/system_analysis/core_analysis.py
@@ -1,193 +1,3 @@
-import os, json
-import nltk
-from nltk import pos_tag, word_tokenize
-from textblob import TextBlob, Sentence, Word, WordList
-from textblob.wordnet import VERB, NOUN, ADJ, ADV
-from textblob.wordnet import Synset
-
-from get_pos import *
-from get_vars import *
-from get_structural_objects import *
-from get_metadata import get_metadata, get_structural_metadata, get_data_from_source
-
-def find_subdict_in_dict(function_dict, found_dict, search_term):
-	for k, v in function_dict.items():
-		if search_term in k:
-			found_dict[k] = v
-		else:
-			if type(v) == list:
-				for value in v:
-					if search_term in value:
-						found_dict[k] = value
-			elif type(v) == str:
-				if search_term in v:
-					found_dict[k] = v
-			elif type(v) == dict:
-				found_dict = find_subdict_in_dict(v, found_dict, search_term)
-	if found_dict:
-		return found_dict
-	return False
-
-def find_functions_in_dict(function_dict, found_functions, search_term):
-	for k, v in function_dict.items():
-		if search_term in k:
-			found_functions.append(k)
-		else:
-			if type(v) == list:
-				for value in v:
-					if search_term in value:
-						found_functions.append(value)
-			elif type(v) == str:
-				if search_term in v:
-					found_functions.append(v)
-			elif type(v) == dict:
-				found_functions = find_functions_in_dict(v, found_functions, search_term)
-	if found_functions:
-		if len(found_functions) > 0:
-			return found_functions
-	return False
-
-def flatten_dict(problem_metadata):
-	flattened = {}
-	for key, values in problem_metadata.items():
-		flattened = iterate_dict(key, values, flattened)
-	if flattened:
-		return flattened
-	return False
-
-def iterate_dict(key, values, flattened):
-	if type(values) == dict:
-		# print('dict values', key, values.keys())
-		for k, v in values.items():
-			if type(v) != dict:
-				flattened[k] = v
-			else:
-				flattened = iterate_dict(k, v, flattened)
-	else:
-		''' to do: add support for nested items other than dicts '''
-		# print('list values', key)
-		flattened[key] = values
-	return flattened
-
-def remove_file(file_path):
-	if os.path.exists(file_path):
-		os.remove(file_path)
-		if os.path.exists(file_path):
-			return False
-	return True
-
-def get_pos(word):
-	tagged = pos_tag(word_tokenize(word))
-	for item in tagged:
-		if 'V' in item[1]:
-			return 'verb'
-		else:
-			return 'noun'
-	return False
-
-def get_data(file_path):
-	if os.path.exists(file_path):
-		objects = None
-		with open(file_path, 'r') as f:
-			objects = json.load(f) if '.json' in file_path else f.read()
-			f.close()
-		if objects:
-			return objects
-	return False
-
-def stringify_metadata(metadata_object):
-	print('function::stringify_metadata')
-	stringified = '_'.join([metadata_object.values()])
-	return stringified
-
-def get_function_in_string(string):
-	print('function::get_function_in_string')
-	found_functions = []
-	function_list = get_function_list()
-	if function_list:
-		words = string.replace(' ','_').split('_')
-		if len(words) > 0:
-			for word in words:
-				if word in function_list:
-					found_functions.append(word)
-	found_functions = set(found_functions)
-	if len(found_functions) > 0:
-		return found_functions
-	return False
-
-def get_objects_in_string(string):
-	''' solution_type = 'balance_info_asymmetry' '''
-	function_list = get_function_list()
-	if function_list:
-		words = string.replace(' ', '_').split('_')
-		if len(words) > 0:
-			objects = []
-			for word in words:
-				pos_type = get_pos(word)
-				if word not in function_list and pos_type != 'verb':
-					objects.append(word)
-			objects = set(objects)
-			if len(objects) > 0:
-				return objects
-	return False
-
-def get_function_list():
-	function_list = []
-	functions = get_data('functions.json')
-	if functions:
-		new_functions = flatten_dict(functions)
-		for key, values in new_functions.items():
-			function_list.append(key)
-			function_list.extend(values)
-		if len(function_list) > 0:
-			function_list = set(function_list)
-			return function_list
-	return False
-
-def get_type_words():
-	print('function::get_type_words')
-	''' return list of abstract/interface/structural words '''
-	type_words = ['info', 'symmetry']
-	return set(type_words)
-
-def convert_sentence(sentence, av):
-    word_map = {}
-    word_map, av = standard_text_processing(sentence, av)
-    if word_map:
-        print('word_map', word_map)
-    row = get_empty_index(av)                   
-    row['line'] = sentence
-    row['word_map'] = word_map
-    row['original_line'] = sentence
-    #row = replace_names(row, av)
-    #row = get_similarity_to_title(title, row)
-    row = get_structural_metadata(row, av)
-    print('metadata', row)
-    
-'''
-av = get_vars()
-for key in av:
-	if 'index' in key:
-		print('\n\nkey', key, av[key])
-
-'''
-'''
-sentence = 'first find clause, then find second'
-convert_sentence(sentence, av)
-
-to do:
-	- fix conjugate
-	- plural: 'line': 'firsts finds claus thens finds seconds'
-	- unaltered: 'operator': {'first find1 clause, then find4 second'}
-
-defs first [
-	'the first element in a countable series'
-]
-defs clause [
-	'(grammar) an expression including a subject and predicate but not constituting a complete sentence'
-]
-'''
-
 #functions = get_data('system_analysis/maps/functions.json')
 #structures = get_data('system_analysis/maps/structures.json')
 core_functions = [
@@ -371,7 +181,7 @@ print('\nmixed_function_object_pairs', mixed_function_object_pairs)
 				- an attribute: 'a tree attribute of the change position object'
 
 
-	- how could you identify useful objects like a pattern intersection, an unenforced rule, or an attribute alignment, once you generate the set of n-degree combinations of core components?
+	- how could you identify useful objects like a pattern intersection, an unenforced rule, or an attribute alignment, once you generate the set of n-degree combinations of core components (in mixed_pairs)?
 
 	- an 'unenforced rule' is an 'attribute object' and would have the definition combinations:
 
@@ -441,21 +251,29 @@ print('\nmixed_function_object_pairs', mixed_function_object_pairs)
 		- a mismatch could be important bc its a common problem type, especially if 'lack of planning' or 'randomness' is a common occurrences 
 			(which we'd know if we derived where randomness could occur given its definition)
 
+		- if you already had 'attribute alignment' as a pattern to look for, or 'intent mismatch' or 'direction conflict' as a specific problem type to identify, you could identify that 
+			a mismatch between randomness injected in an ordered system and the intent of ordering a system was an indicator of the importance of the core object combination
+
+		- you could also use insights like 'subsets of a system should be similar to the system' 
+			(so the metadata of system components (intent to disorder a system) should be similar to the metadata of the system (intent to order a system))
+
+		- as a backup, you could identify that the randomness invalidates the object intent, which seems like an indicator that it shouldn't happen
+
 		- edge case: if your function is to generate randomness, this process would have to examine other metadata, to find that:
 			- a partially implemented randomness function (with errors) doesnt generate the target randomness type (evenly distributed output probabilities)
 
 
-		- the general overview of this method to filter the most important objects in core object combinations is:
+	- the general overview of this method to filter the most important objects in core object combinations is:
 
-			- derive metadata of the core object combination (subset rule) with system analysis
-			- find & iterate through structures of core object combination intents (structure in which it would be found, like 'partially implemented function')
-			- find & iterate through metadata of structures
-				- target intents like 'function-building' have intents like 'reducing manmade error')
-				- causes like 'lack of organization'
-			- pull any related definitions (randomness definition)
-			- iterate through problem types like mismatch, and iterate through metadata of the objects, looking for inconsistencies, assuming that the structure is intentional 
-				(youd intentionally implement a 'partially implemented function' in order to do what intents - to 'inject randomness in an ordered system'?
-			- check if each problem type instance found is useful in some way (does it generate randomness, and is that the intent of the function?)
-			- if not, and if this is an important problem type to avoid, the object 'subset rule' is probably an important specific instance of that problem type 
-				('function missing validation', or an 'unenforced rule')
+		- derive metadata of the core object combination (subset rule) with system analysis
+		- find & iterate through structures of core object combination intents (structure in which it would be found, like 'partially implemented function')
+		- find & iterate through metadata of structures
+			- target intents like 'function-building' have intents like 'reducing manmade error')
+			- causes like 'lack of organization'
+		- pull any related definitions (randomness definition)
+		- iterate through problem types like mismatch, and iterate through metadata of the objects, looking for inconsistencies, assuming that the structure is intentional 
+			(youd intentionally implement a 'partially implemented function' in order to do what intents - to 'inject randomness in an ordered system'?
+		- check if each problem type instance found is useful in some way (does it generate randomness, and is that the intent of the function?)
+		- if not, and if this is an important problem type to avoid, the object 'subset rule' is probably an important specific instance of that problem type 
+			('function missing validation', or an 'unenforced rule')
 '''
