6e1b043e6cb775652f3ecba2ac9355a7dc5ffa0d
outdreamer <jonijezewski@gmail.com>
2020-05-29 14:41:26 -0700

    add notes

--- repeated_lines.py
+++ repeated_lines.py
@@ -7,6 +7,7 @@ cwd = cwd.replace('find_existing_solutions','')
 path = ''.join([cwd, 'docs'])
 print('path', path)
 
+'''
 for dirname in os.listdir(path):
 	print('dirname', dirname)
 	full_dirname = ''.join([path, '/', dirname])
@@ -39,7 +40,43 @@ for line in new_repeated_lines:
 	print(line, new_repeated_lines[line])
 
 
-''' compile function names '''
+# compile function names
 import subprocess
 cmd = ["""grep -r 'def ' . --include='*.py' >> code_function_names.txt"""]
 subprocess.check_output(cmd,shell=True)
+
+'''
+all_lines = []
+
+path = os.getcwd()
+print('path', path)
+for dirname in os.listdir(path):
+	print('dirname', dirname)
+	full_dirname = ''.join([path, '/', dirname])
+	if os.path.isdir(full_dirname):
+		if 'find_existing_solutions' in dirname or 'planned_features' in dirname or 'docs' in dirname:
+			for cur, _dirs, files in os.walk(full_dirname):
+				for filename in files:
+					full_filename = ''.join([cur, '/', filename])
+					if '.py' in filename or '.json' in filename or '.txt' in filename or '.md' in filename or '.svg' in filename:
+						print('full_filename', full_filename)
+						with open(full_filename, 'r') as f:
+							lines = f.readlines()
+							all_lines.append('\n********** SOF **********\n')
+							all_lines.append(''.join(['\n********** ', full_filename, ' **********\n']))
+							all_lines.extend(lines)
+							all_lines.append('\n********** EOF **********\n')
+	else:
+		if '.py' in full_dirname or '.json' in full_dirname or '.txt' in full_dirname or '.md' in full_dirname or '.svg' in full_dirname:
+			with open(full_dirname, 'r') as f:
+				lines = f.readlines()
+				all_lines.append('\n********** SOF **********\n')
+				all_lines.append(''.join(['\n********** ', full_dirname, ' **********\n']))
+				all_lines.extend(lines)
+				all_lines.append('\n********** EOF **********\n')	
+
+print('len', len(all_lines))
+if len(all_lines) > 0:
+	with open('log.txt', 'w') as f:
+		f.write(''.join(all_lines))
+		f.close()
