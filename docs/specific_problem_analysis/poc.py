c84deb38af558f33ba5e0b76136e890050ac43f4
outdreamer <jonijezewski@gmail.com>
2020-09-04 07:24:31 -0700

    add poc notes

---
+++ docs/specific_problem_analysis/poc.py
@@ -0,0 +1,36 @@
+''' poc system object identification:
+
+- import system as network
+
+- pull efficiency definition: 
+
+	- interaction that fulfills an original intent with higher or similar resources after units of work, compared to units of work
+
+	- pull sub-object definitions
+		- interaction: function connecting multiple objects
+		- cost: unit of work
+		- benefit: resource that can be an input for an intent
+
+- convert definitions to system interface
+
+	- find routes that 'fulfills original intent with higher or similar resources after units of work, compared to units of work'
+
+	- convert sub-objects to system interface
+		- resources in the system
+		- intents supported by the system
+		- original intent of the system
+		- units of work in the system
+
+- create combination list of objects in system
+	
+	- resource A + function B for intent C
+	- function A + object B + function C for intent 'get function C output D'
+
+- identify objects or combinations matching converted structural definition routes
+
+	- does system object combination 'resource A + function B for intent C' match the efficiency definition 'high benefit/cost ratio' in its structural version 'high resource output compared to work input'?
+		- does it have an optimization opportunity (does it match an inefficiency structural definition)?
+			- if so, how could that inefficiency be converted to an inefficiency?
+				- apply all mechanisms to reduce work units that could result in higher resource outputs
+
+'''
